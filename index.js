73bdb388fdd1d556b90b84479b55814c99988e08
estebanCesave <francisco.flores@cesaveson.com>
2023-02-18 17:49:25 -0700

    Added a procfile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler} = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
